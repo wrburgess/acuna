4a211085b615c213fb5702874af948040f33a62a
Randy Burgess <wrburgess@gmail.com>
2025-04-20 20:19:07 -0500

    WIP

---
+++ app/models/admin/comment.rb
@@ -0,0 +1,21 @@
+# app/models/admin/comment.rb
+
+module Admin
+  class Comment < ApplicationRecord
+    include Loggable
+    include Archivable
+
+    belongs_to :commentable, polymorphic: true
+    belongs_to :user
+
+    validates :body, presence: true
+
+    # Scopes
+    scope :active, -> { where(archived_at: nil) }
+
+    # Methods
+    def truncated_body
+      body.truncate(100)
+    end
+  end
+end
